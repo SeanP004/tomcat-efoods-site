3c661a0e17517c071616cdb45e25e0ad825d2e88
Vincent Chu <vince.chu@live.ca>
2014-11-08 13:54:38 -0500

    Added Database Pool Connection Context (context.xml) Added Front Controller.

---
+++ ClientSite/src/ctrl/Main.java
@@ -0,0 +1,43 @@
+package ctrl;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/")
+public class Main extends HttpServlet {
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+} // Main Servlet
