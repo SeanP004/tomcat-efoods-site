fd1aeac86a0acfeb8d395907c4d209b626120860
Vincent Chu <vince.chu@live.ca>
2014-11-08 22:32:37 -0500

    Added DAO interface and Option classes.

---
+++ ClientSite/src/model/DAO.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.*;
+
+public interface DAO {
+    List<ItemBean>     getItems(GetItemOption options);
+    List<CategoryBean> getCategories(GetCategoryOption options);
+    ItemBean           getItemById(String id);
+}
+
+class GetItemOption {
+    String  orderBy    = "name";  // Ordered by the item name
+    int     offset     = -1;      // No offset set
+    int     fetch      = -1;      // Unlimited page size
+    String  category   = null;    // No category filter
+    String  searchTerm = null;    // No search filter
+    double  minPrice   = -1;      // No minimum price filter
+    double  maxPrice   = -1;      // No maximum price filter
+}
+
+class GetCategoryOption {
+    String  orderBy    = "name";  // Ordered by the category name
+    int     offset     = -1;      // No offset set
+    int     fetch      = -1;      // Unlimited page size
+    String  searchTerm = null;    // No search filter
+}
