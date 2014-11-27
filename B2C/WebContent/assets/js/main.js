03d5b1a53387f4874847d6393192cd1dc9b54555
Vincent Chu <vince.chu@live.ca>
2014-11-27 01:56:56 -0500

    Dynamic search on catalog page.

--- B2C/WebContent/assets/js/main.js
+++ B2C/WebContent/assets/js/main.js
@@ -518,7 +518,8 @@ define('Search', ['Element', 'Elements', 'Ajax', 'FormData', 'Template'],
 
         if (!$('.search-filters').elem()) {return;}
 
-        var searchTabs    = $$('.search-filters .nav-tabs li a')
+        var searchUri     = '/eFoods/browse?'
+          , searchTabs    = $$('.search-filters .nav-tabs li a')
           , searchMore    = $('.search-filters .nav-tabs li a[data-id="more"]').parent()
           , searchOpts    = $('.search-options')
           , searchTerm    = $('input[name="searchTerm"]')
@@ -530,16 +531,52 @@ define('Search', ['Element', 'Elements', 'Ajax', 'FormData', 'Template'],
           , maxPriceLabel = $('input[name="maxPrice"] + span')
           , closeBtn      = $('.search-options .close-btn .btn')
           , submit        = $('.search-box button[type="submit"]')
+          , searchForm    = $('form[role="search"]')
+          , searchResults = $('.catalog')
           ;
 
         function toggleAdvancedSearch(toggle) {
             if (!toggle || searchMore.hasClass('active')) {
                 searchMore.removeClass('active');
                 searchOpts.addClass('hidden');
+                if (toggle) {
+                    searchTabs.each(function (el) {
+                        if (el.attr('data-id') === category.val()) {
+                            el.parent().addClass('active');
+                        }
+                    });
+                }
             } else if (toggle) {
                 searchMore.addClass('active');
                 searchOpts.removeClass('hidden');
             }
+        }
+
+        function showError(error) {
+            $('.error')
+                .html(error)
+                .addClass('alert')
+                .addClass('alert-danger')
+                .removeClass('hidden');
+        }
+
+        function updateSearchResults() {
+            var form = $form(searchForm.elem());
+            window.history.pushState(null, $('title').html(), searchUri + form.encode());
+            $http
+                .get(searchUri + form.encode())
+                .onError(showError)
+                .onSuccess(function (data) {
+                    var doc = (new DOMParser()).parseFromString(data, "text/html");
+                    var error = $('.error', doc).html();
+
+                    if (!error) {
+                        searchResults.html($('.catalog', doc).html());
+                        $('.error').html('').addClass('hidden');
+                    } else {
+                        showError(error);
+                    }
+                });
         }
 
         closeBtn.on('click', function () {
@@ -556,20 +593,20 @@ define('Search', ['Element', 'Elements', 'Ajax', 'FormData', 'Template'],
             if (this.attr('data-id') !== 'more') {
                 parent.addClass('active');
                 category.val(this.attr('data-id'));
+                updateSearchResults();
             }
         });
 
-        minPrice.on('focus', function () {
-            minPriceLabel.html(minPrice.val());
-        });
-        maxPrice.on('focus', function () {
-            maxPriceLabel.html(maxPrice.val());
-        });
+        searchTerm.on('keyup', updateSearchResults);
+        category.on('change', updateSearchResults);
+        orderBy.on('change', updateSearchResults);
         minPrice.on('change', function () {
             minPriceLabel.html(minPrice.val());
+            updateSearchResults();
         });
         maxPrice.on('change', function () {
             maxPriceLabel.html(maxPrice.val());
+            updateSearchResults();
         });
 
     }
--- B2C/web-src/js/search.js
+++ B2C/web-src/js/search.js
@@ -5,7 +5,8 @@ define('Search', ['Element', 'Elements', 'Ajax', 'FormData', 'Template'],
 
         if (!$('.search-filters').elem()) {return;}
 
-        var searchTabs    = $$('.search-filters .nav-tabs li a')
+        var searchUri     = '/eFoods/browse?'
+          , searchTabs    = $$('.search-filters .nav-tabs li a')
           , searchMore    = $('.search-filters .nav-tabs li a[data-id="more"]').parent()
           , searchOpts    = $('.search-options')
           , searchTerm    = $('input[name="searchTerm"]')
@@ -17,16 +18,52 @@ define('Search', ['Element', 'Elements', 'Ajax', 'FormData', 'Template'],
           , maxPriceLabel = $('input[name="maxPrice"] + span')
           , closeBtn      = $('.search-options .close-btn .btn')
           , submit        = $('.search-box button[type="submit"]')
+          , searchForm    = $('form[role="search"]')
+          , searchResults = $('.catalog')
           ;
 
         function toggleAdvancedSearch(toggle) {
             if (!toggle || searchMore.hasClass('active')) {
                 searchMore.removeClass('active');
                 searchOpts.addClass('hidden');
+                if (toggle) {
+                    searchTabs.each(function (el) {
+                        if (el.attr('data-id') === category.val()) {
+                            el.parent().addClass('active');
+                        }
+                    });
+                }
             } else if (toggle) {
                 searchMore.addClass('active');
                 searchOpts.removeClass('hidden');
             }
+        }
+
+        function showError(error) {
+            $('.error')
+                .html(error)
+                .addClass('alert')
+                .addClass('alert-danger')
+                .removeClass('hidden');
+        }
+
+        function updateSearchResults() {
+            var form = $form(searchForm.elem());
+            window.history.pushState(null, $('title').html(), searchUri + form.encode());
+            $http
+                .get(searchUri + form.encode())
+                .onError(showError)
+                .onSuccess(function (data) {
+                    var doc = (new DOMParser()).parseFromString(data, "text/html");
+                    var error = $('.error', doc).html();
+
+                    if (!error) {
+                        searchResults.html($('.catalog', doc).html());
+                        $('.error').html('').addClass('hidden');
+                    } else {
+                        showError(error);
+                    }
+                });
         }
 
         closeBtn.on('click', function () {
@@ -43,23 +80,21 @@ define('Search', ['Element', 'Elements', 'Ajax', 'FormData', 'Template'],
             if (this.attr('data-id') !== 'more') {
                 parent.addClass('active');
                 category.val(this.attr('data-id'));
+                updateSearchResults();
             }
         });
 
+        searchTerm.on('keyup', updateSearchResults);
+        category.on('change', updateSearchResults);
+        orderBy.on('change', updateSearchResults);
         minPrice.on('change', function () {
-            if (minPrice.val() >= maxPrice.val()) {
-                // TODO show error
-            }
             minPriceLabel.html(minPrice.val());
+            updateSearchResults();
         });
         maxPrice.on('change', function () {
-            if (minPrice.val() >= maxPrice.val()) {
-                // TODO show error
-            }
             maxPriceLabel.html(maxPrice.val());
+            updateSearchResults();
         });
-
-
 
     }
 );
